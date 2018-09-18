c7045c4ec1f9a72b10d7ec36475d59f333454f85
Sam Oluoch <samsoluoch@gmail.com>
2018-09-18 14:11:23 +0300

    added some styles

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,15 +12,8 @@ def login():
     This is a user login route that allows users to login
     '''
     login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
-
-    title = "User Login"
+    title = "Coachya"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
